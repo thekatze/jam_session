606c831c105a3b20b0864cf1cdf0c1032d6e65e2
Pascal Blessing <pblessing.mmt-b2017@fh-salzburg.ac.at>
2023-11-18 00:03:02 +0100

    add multiplayer

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var player_id : int = 0
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -14,12 +15,12 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump_%s" % player_id) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left_%s" % player_id, "move_right_%s" % player_id)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
