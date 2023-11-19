5c8ef348c58e7d2f8d68bc967c1671c09b7a9fbb
Pascal Blessing <pblessing.mmt-b2017@fh-salzburg.ac.at>
2023-11-19 18:51:26 +0100

    make whole win text colorful

--- scenes/level.gd
+++ scenes/level.gd
@@ -3,6 +3,6 @@ extends Control
 
 func _on_bread_player_finished(player_id):
 	$"UI/WinPanel/HBoxContainer/PlayerId Text".text = str(player_id + 1)
-	$"UI/WinPanel/HBoxContainer/PlayerId Text".modulate = player_colors[player_id]
+	$"UI/WinPanel/HBoxContainer".modulate = player_colors[player_id]
 	$UI/WinPanel.visible = true
 	Engine.time_scale = 0
