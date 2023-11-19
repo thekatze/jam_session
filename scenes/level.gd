fc296f9852ae396781eedb974e0302cfd5420cb3
Leo Katzengruber <leofel.katz@gmail.com>
2023-11-19 18:18:33 +0100

    color player number in victory screen

--- scenes/level.gd
+++ scenes/level.gd
@@ -1,7 +1,8 @@
 extends Control
-
+@export var player_colors : PackedColorArray
 
 func _on_bread_player_finished(player_id):
 	$"UI/WinPanel/HBoxContainer/PlayerId Text".text = str(player_id + 1)
+	$"UI/WinPanel/HBoxContainer/PlayerId Text".modulate = player_colors[player_id]
 	$UI/WinPanel.visible = true
 	Engine.time_scale = 0
