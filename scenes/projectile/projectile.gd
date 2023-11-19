769f9463e7c4102e438ce20468393ff7417ebeac
Leo Katzengruber <leofel.katz@gmail.com>
2023-11-19 18:45:04 +0100

    fix build & projectile texture

--- scenes/projectile/projectile.gd
+++ scenes/projectile/projectile.gd
@@ -2,6 +2,9 @@ extends RigidBody2D
 
 var push_velocity : Vector2
 
+func _process(delta):
+	$Sprite2D.rotate(10 * delta)
+
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		queue_free()
